f7c12ba6ff098c784e076f12f9b97a674a4012b1
Christian Berndt <berndt@inofix.ch>
2017-08-24 12:19:40 +0200

    Initial checkin of api and service module.

---
+++ data-manager-service/src/main/java/ch/inofix/data/service/impl/MeasurementServiceImpl.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package ch.inofix.data.service.impl;
+
+import ch.inofix.data.service.base.MeasurementServiceBaseImpl;
+
+/**
+ * The implementation of the measurement remote service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link ch.inofix.data.service.MeasurementService} interface.
+ *
+ * <p>
+ * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
+ * </p>
+ *
+ * @author Christian Berndt
+ * @see MeasurementServiceBaseImpl
+ * @see ch.inofix.data.service.MeasurementServiceUtil
+ */
+public class MeasurementServiceImpl extends MeasurementServiceBaseImpl {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. Always use {@link ch.inofix.data.service.MeasurementServiceUtil} to access the measurement remote service.
+	 */
+}
