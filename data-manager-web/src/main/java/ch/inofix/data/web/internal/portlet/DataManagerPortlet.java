f1dffb22f9e0b75bc627cf91941100aecfa23733
Christian Berndt <berndt@inofix.ch>
2017-08-29 16:47:34 +0200

    Update naming.

--- data-manager-web/src/main/java/ch/inofix/data/web/internal/portlet/DataManagerPortlet.java
+++ data-manager-web/src/main/java/ch/inofix/data/web/internal/portlet/DataManagerPortlet.java
@@ -16,7 +16,7 @@ import org.osgi.service.component.annotations.Component;
 	property = {
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=data-manager-web Portlet",
+		"javax.portlet.display-name=Data Manager",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + DataManagerPortletKeys.DataManager,
