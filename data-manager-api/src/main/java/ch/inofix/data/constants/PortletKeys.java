33ba2be9ee13764fca90e9fa4666bebfd53277a4
Christian Berndt <berndt@inofix.ch>
2017-09-10 16:34:52 +0200

    Setup DataManagerPortlet.

---
+++ data-manager-api/src/main/java/ch/inofix/data/constants/PortletKeys.java
@@ -0,0 +1,15 @@
+package ch.inofix.data.constants;
+
+/**
+ * 
+ * @author Christian Berndt
+ * @created 2017-09-10 16:21
+ * @modified 2017-09-10 16:21
+ * @version 1.0.0
+ *
+ */
+public class PortletKeys {
+
+    public static final String DATA_MANAGER = "ch_inofix_data_web_internal_portlet_DataManagerPortlet";
+
+}
