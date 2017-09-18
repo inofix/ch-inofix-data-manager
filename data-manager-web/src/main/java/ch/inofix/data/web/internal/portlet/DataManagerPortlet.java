4baec0b5b0cd49f0c829831e97ec7212eba58d0a
Christian Berndt <berndt@inofix.ch>
2017-09-18 11:23:49 +0200

    Add measurement GUI.

--- data-manager-web/src/main/java/ch/inofix/data/web/internal/portlet/DataManagerPortlet.java
+++ data-manager-web/src/main/java/ch/inofix/data/web/internal/portlet/DataManagerPortlet.java
@@ -13,22 +13,23 @@ import org.osgi.service.component.annotations.Component;
  *
  * @author Christian Berndt
  * @created 2017-09-10 16:32
- * @modified 2017-09-10 16:32
- * @version 1.0.0
+ * @modified 2017-09-18 11:20
+ * @version 1.0.1
  */
 @Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=category.inofix",
-		"com.liferay.portlet.instanceable=false",
-		"javax.portlet.display-name=Data Manager",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + PortletKeys.DATA_MANAGER,
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
+    immediate = true,
+    property = {
+        "com.liferay.portlet.css-class-wrapper=portlet-data-manager",
+        "com.liferay.portlet.display-category=category.inofix",
+        "com.liferay.portlet.instanceable=false",
+        "javax.portlet.display-name=Data Manager",
+        "javax.portlet.init-param.template-path=/",
+        "javax.portlet.init-param.view-template=/view.jsp",
+        "javax.portlet.name=" + PortletKeys.DATA_MANAGER,
+        "javax.portlet.resource-bundle=content.Language",
+        "javax.portlet.security-role-ref=power-user,user"
+    },
+    service = Portlet.class
 )
 public class DataManagerPortlet extends MVCPortlet {
 }
---
+++ data-manager-web/src/main/resources/META-INF/resources/edit_measurement.jsp
@@ -0,0 +1,8 @@
+<%--
+    edit_measurement.jsp: edit a single measurement.
+
+    Created:     2017-09-18 11:17 by Christian Berndt
+    Modified:    2017-09-18 11:17 by Christian Berndt
+    Version:     0.1.0
+--%>
+edit_measurement.jsp: TODO
