af9d371e317152f41781b957d5a53aaad57ad592
Christian Berndt <berndt@inofix.ch>
2017-11-13 18:05:03 +0100

    Dynamic property maps.

--- data-manager-service/src/main/java/ch/inofix/data/model/impl/MeasurementImpl.java
+++ data-manager-service/src/main/java/ch/inofix/data/model/impl/MeasurementImpl.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-present Inofix GmbH, Luzern. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -14,6 +14,17 @@
 
 package ch.inofix.data.model.impl;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+
 import aQute.bnd.annotation.ProviderType;
 
 /**
@@ -24,14 +35,48 @@ import aQute.bnd.annotation.ProviderType;
  * </p>
  *
  * @author Christian Berndt
+ * @created 2017-08-24 12:19
+ * @modified 2017-11-13 17:32
+ * @version 1.0.1
  */
 @ProviderType
 public class MeasurementImpl extends MeasurementBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a measurement model instance should use the {@link ch.inofix.data.model.Measurement} interface instead.
-	 */
-	public MeasurementImpl() {
-	}
+    /*
+     * NOTE FOR DEVELOPERS:
+     *
+     * Never reference this class directly. All methods that expect a
+     * measurement model instance should use the {@link
+     * ch.inofix.data.model.Measurement} interface instead.
+     */
+    public MeasurementImpl() {
+    }
+
+    public Map<String, String> getDataMap() {
+
+        JSONObject jsonObject = null;
+        Map<String, String> map = new HashMap<String, String>();
+
+        try {
+            jsonObject = JSONFactoryUtil.createJSONObject(GetterUtil.getString(getData()));
+
+            Iterator<String> keys = jsonObject.keys();
+
+            while (keys.hasNext()) {
+
+                String key = keys.next();
+
+                map.put(key, jsonObject.getString(key));
+
+            }
+
+        } catch (JSONException e) {
+            _log.error(e);
+        }
+
+        return map;
+
+    }
+
+    private static Log _log = LogFactoryUtil.getLog(MeasurementImpl.class.getName());
+
 }
