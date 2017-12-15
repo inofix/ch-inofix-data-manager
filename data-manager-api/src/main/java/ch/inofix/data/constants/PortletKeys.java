861fbf16b3e7d669a5eb98bbad80c8d9b0b6bdbd
Christian Berndt <berndt@inofix.ch>
2017-12-15 23:37:05 +0100

    Extend from core PortletKeys.

--- data-manager-api/src/main/java/ch/inofix/data/constants/PortletKeys.java
+++ data-manager-api/src/main/java/ch/inofix/data/constants/PortletKeys.java
@@ -4,11 +4,11 @@ package ch.inofix.data.constants;
  * 
  * @author Christian Berndt
  * @created 2017-09-10 16:21
- * @modified 2017-09-10 16:21
- * @version 1.0.0
+ * @modified 2017-12-14 23:17
+ * @version 1.0.1
  *
  */
-public class PortletKeys {
+public class PortletKeys extends com.liferay.portal.kernel.util.PortletKeys {
 
     public static final String DATA_MANAGER = "ch_inofix_data_web_internal_portlet_DataManagerPortlet";
 
