547d203806b6196fba75394a7d053d78c56fbbac
Christian Berndt <berndt@inofix.ch>
2017-12-25 17:58:01 +0100

    Removed obsolete click me method.

--- data-manager-web/src/main/resources/META-INF/resources/lib/index.es.js
+++ data-manager-web/src/main/resources/META-INF/resources/lib/index.es.js
@@ -1,25 +1,15 @@
 import * as d3 from "d3";
 
 export default function() {
-    
- // DOM API
- var clickMe = document.getElementById('click-me');
- clickMe.onclick = function() {
-   if (this.style.backgroundColor) {
-     this.style.backgroundColor = '';
-   } else {
-     this.style.backgroundColor = 'red';
-   }
- }
 
- // D3 Selection API. Note: it attaches the
- // callbacks to each element in the selection
- d3.selectAll('.hover-me')
-   .on('mouseover', function() {
-     this.style.backgroundColor = 'yellow';
-   })
-   .on('mouseleave', function() {
-     this.style.backgroundColor = '';
-   });
+     // D3 Selection API. Note: it attaches the
+     // callbacks to each element in the selection
+     d3.selectAll('.hover-me')
+     .on('mouseover', function() {
+         this.style.backgroundColor = 'yellow';
+     })
+     .on('mouseleave', function() {
+         this.style.backgroundColor = '';
+     });
 
 }
